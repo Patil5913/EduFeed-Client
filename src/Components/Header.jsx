0e61f53eabaf841b643591fe9d0c7f2929e3b84e
parth <contactparth2664@gmail.com>
2024-04-17 23:22:23 +0530

    Add dependencies and update logout functionality

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,6 +10,12 @@ const Header = () => {
     // Assuming the main page is "/" and login page is "/login"
     return !(location.pathname === "/" || location.pathname === "/login" || location.pathname === "/forgot");
   };
+  const handleClick = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("accessToken")
+    localStorage.removeItem("role");
+    window.location.href = "/";
+  };
 
   return (
     <header className="mb-1 my-3">
@@ -20,12 +26,12 @@ const Header = () => {
         </a>
         <div className="flex w-full justify-end gap-4">
           {shouldShowLoginButton() ? (
-            <Link
-              to="/logout"
+            <button
+              onClick={handleClick}
               className="relative border-2 border-dashed border-quaternary px-4 py-2 lg:px-10 lg:py-3 text-quaternary text-base font-bold overflow-hidden bg-transparent rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#E5E3D4] before:to-[#c3ba76] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
             >
               Logout
-            </Link>
+            </button>
           ) : (
             <Link
               to="/login"
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,18 +1,70 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  
   const [showPassword, setShowPassword] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword((prevState) => !prevState);
   };
 
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://edufeed-backend.vercel.app/api/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        const accessToken = data.accessToken;
+        const role = data.role;
+        localStorage.setItem("token", accessToken);
+        localStorage.setItem("role", role);
+        if(role === "student") {
+          navigate("/student")
+        } else if(role === "mentor") {
+          navigate("/mentor")
+        }
+        else if(role === "authority") {
+          navigate("/authority")
+        }
+        else {
+          console.error("Role not found!");
+        }
+      } else {
+        // Handle failed login
+        console.error("Login failed!");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <>
       <div className=" flex-grow  w-full my-5 p-8 rounded-lg  flex justify-center items-center">
         <form
-          action=""
+          onSubmit={handleSubmit}
           className="flex flex-col justify-center items-center  w-full sm:w-fit px-8 pb-6 "
         >
           <div className=" ">
@@ -34,6 +86,7 @@ const Login = () => {
           <div className="py-8 text-base leading-6 space-y-6 text-gray-700 sm:text-lg sm:leading-7 w-full sm:px-5 ">
             <div className="relative">
               <input
+                onChange={handleChange}
                 autoComplete="off"
                 id="email"
                 name="email"
@@ -50,27 +103,33 @@ const Login = () => {
             </div>
             {/* Password Input */}
             <div className="relative">
-  <input
-    autoComplete="off"
-    id="password"
-    name="password"
-    type={showPassword ? "text" : "password"}
-    className="bg-primary peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
-    placeholder="Password"
-  />
-  <label
-    htmlFor="password"
-    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-  >
-    Password
-  </label>
-  {showPassword ? (
-    <i className="ri-eye-fill absolute right-0 top-3 transform translate-x-[-0.5rem] cursor-pointer" onClick={togglePasswordVisibility}></i>
-  ) : (
-    <i className="ri-eye-off-fill absolute right-0 top-3 transform translate-x-[-0.5rem] cursor-pointer" onClick={togglePasswordVisibility}></i>
-  )}
-</div>
-
+              <input
+                onChange={handleChange}
+                autoComplete="off"
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                className="bg-primary peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
+                placeholder="Password"
+              />
+              <label
+                htmlFor="password"
+                className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+              >
+                Password
+              </label>
+              {showPassword ? (
+                <i
+                  className="ri-eye-fill absolute right-0 top-3 transform translate-x-[-0.5rem] cursor-pointer"
+                  onClick={togglePasswordVisibility}
+                ></i>
+              ) : (
+                <i
+                  className="ri-eye-off-fill absolute right-0 top-3 transform translate-x-[-0.5rem] cursor-pointer"
+                  onClick={togglePasswordVisibility}
+                ></i>
+              )}
+            </div>
 
             <a
               href="/forgot"
@@ -79,12 +138,12 @@ const Login = () => {
               Forgot password?
             </a>
           </div>
-          <Link
-            to=""
+          <button
+            type="submit"
             className="hover:brightness-110  font-bold py-3 px-6 rounded-full bg-gradient-to-r from-[#E5E3D4] to-[#c3ba76] text-quaternary"
           >
             Login
-          </Link>
+          </button>
         </form>
       </div>
     </>
