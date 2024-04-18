357a3394326612c5bb3271b47beacb2e81e9a0a3
parth <contactparth2664@gmail.com>
2024-04-18 14:29:18 +0530

    Add react-toastify dependency

--- src/Pages/Mentor/Register.jsx
+++ src/Pages/Mentor/Register.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "remixicon/fonts/remixicon.css";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Link } from "react-router-dom";
 
 const Register = () => {
@@ -8,11 +10,72 @@ const Register = () => {
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    prn: "",
+    role: "",
+    branch: "",
+    currentsem: "",
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://edufeed-backend.vercel.app/api/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      if (response.status === 200) {
+        const data = await response.json();
+        toast.success("User registered successfully!");
+        console.log(data);
+      }
+      else if(response.status === 208) {
+        toast.error("User already exists!");
+      } 
+      else {
+        console.error("Registration failed!");
+        toast.error("Registration failed!");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  const handleUpload = async (e) => {
+    const file = e.target.files[0];
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const response = await fetch("https://edufeed-backend.vercel.app/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+      if (response.ok) {
+        console.log("File uploaded successfully");
+      } else {
+        console.error("Failed to upload file");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <>
       <div className=" w-full  py-2  flex justify-center flex-col items-center">
         <div className="w-full flex flex-col justify-center items-center space-y-4">
-        <i class="ri-user-add-fill text-[2rem]"></i>
+          <i class="ri-user-add-fill text-[2rem]"></i>
           <h1 className="text-xl lg:text-2xl font-serif text-quaternary font-bold">
             Register
           </h1>
@@ -24,15 +87,16 @@ const Register = () => {
           <div class="  py-8 px-8 text-base leading-6 space-y-6 text-gray-700 sm:text-lg sm:leading-7 w-full sm:px-5 ">
             <div class="relative">
               <input
+                onChange={handleChange}
                 autocomplete="off"
-                id="Name"
-                name="Name"
+                id="name"
+                name="name"
                 type="text"
                 class=" bg-transparent peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-xs  text-gray-900 focus:outline-none focus:border-secondary"
-                placeholder="Name"
+                placeholder="name"
               />
               <label
-                for="Name"
+                for="name"
                 class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
               >
                 Name
@@ -40,15 +104,16 @@ const Register = () => {
             </div>
             <div class="relative">
               <input
+                onChange={handleChange}
                 autocomplete="off"
-                id="Prn.no"
-                name="Prn.no"
+                id="prn"
+                name="prn"
                 type="text"
                 class=" bg-transparent peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-xs  text-gray-900 focus:outline-none focus:border-secondary"
-                placeholder="Prn.no"
+                placeholder="prn"
               />
               <label
-                for="Prn.no"
+                for="prn"
                 class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
               >
                 Prn.no
@@ -59,6 +124,7 @@ const Register = () => {
           <div class="  sm:pt-14 px-8 text-base leading-6 space-y-6 text-gray-700 sm:text-lg sm:leading-7 w-full sm:px-5 ">
             <div class="relative">
               <input
+                onChange={handleChange}
                 autocomplete="off"
                 id="email"
                 name="email"
@@ -74,28 +140,33 @@ const Register = () => {
               </label>
             </div>
             <div className="relative">
-      <input
-        autoComplete="off"
-        id="password"
-        name="password"
-        type={showPassword ? "text" : "password"}
-        className="bg-transparent peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-xs text-gray-900 focus:outline-none focus:border-secondary"
-        placeholder="Password"
-      />
-      <label
-        htmlFor="password"
-        className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-      >
-        Password
-      </label>
-      {showPassword ? (
-    <i className="ri-eye-fill absolute right-0 top-3 transform translate-x-[-0.5rem] cursor-pointer" onClick={togglePasswordVisibility}></i>
-  ) : (
-    <i className="ri-eye-off-fill absolute right-0 top-3 transform translate-x-[-0.5rem] cursor-pointer" onClick={togglePasswordVisibility}></i>
-  )}
-    </div>
-
-           
+              <input
+                onChange={handleChange}
+                autoComplete="off"
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                className="bg-transparent peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-xs text-gray-900 focus:outline-none focus:border-secondary"
+                placeholder="Password"
+              />
+              <label
+                htmlFor="password"
+                className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+              >
+                Password
+              </label>
+              {showPassword ? (
+                <i
+                  className="ri-eye-fill absolute right-0 top-3 transform translate-x-[-0.5rem] cursor-pointer"
+                  onClick={togglePasswordVisibility}
+                ></i>
+              ) : (
+                <i
+                  className="ri-eye-off-fill absolute right-0 top-3 transform translate-x-[-0.5rem] cursor-pointer"
+                  onClick={togglePasswordVisibility}
+                ></i>
+              )}
+            </div>
 
             <div class="relative inline-flex w-full">
               <svg
@@ -110,14 +181,17 @@ const Register = () => {
                 />
               </svg>
               <select
-                id="Role"
-                name="Role"
+                onChange={handleChange}
+                id="role"
+                name="role"
                 class="border-b-2 text-gray-600 h-10  pr-10 bg-transparent hover:border-gray-400 focus:outline-none focus:border-secondary appearance-none w-full"
               >
-                <option className="font-bold text-xl">Role</option>
-                <option>Student</option>
-                <option>Mentor</option>
-                <option>Authority</option>
+                <option value="" className="font-bold text-xl">
+                  Role
+                </option>
+                <option value="student">Student</option>
+                <option value="mentor">mentor</option>
+                <option value="authority">Authority</option>{" "}
               </select>
             </div>
           </div>
@@ -136,8 +210,9 @@ const Register = () => {
                 />
               </svg>
               <select
-                id="Role"
-                name="Role"
+                onChange={handleChange}
+                id="branch"
+                name="branch"
                 class="border-b-2 text-gray-600 h-10  pr-10 bg-transparent hover:border-gray-400 focus:outline-none focus:border-secondary appearance-none w-full"
               >
                 <option className="font-bold text-xl ">Branch</option>
@@ -162,8 +237,9 @@ const Register = () => {
                 />
               </svg>
               <select
-                id="Role"
-                name="Role"
+                onChange={handleChange}
+                id="currentsem"
+                name="currentsem"
                 class="border-b-2 text-gray-600 h-10  pr-10 bg-transparent hover:border-gray-400 focus:outline-none focus:border-secondary appearance-none w-full"
               >
                 <option className="font-bold"> currentsem</option>
@@ -179,14 +255,15 @@ const Register = () => {
             </div>
           </div>
         </form>
+        <ToastContainer />
         <div className=" sm:w-3/4  items-center justify-between pb-16 sm:pb-0  space-y-4 sm:space-y-0">
           <div className="flex justify-center  w-full ">
-            <Link
-              to=""
+            <button
+              onClick={handleSubmit}
               className="hover:brightness-110  font-bold py-3 px-6 rounded-full bg-gradient-to-r from-[#E5E3D4] to-[#c3ba76] text-quaternary"
             >
               Submit
-            </Link>
+            </button>
           </div>
 
           <div class=" flex justify-end ">
@@ -218,14 +295,13 @@ const Register = () => {
                     Cvv (MAX. 800x400px)
                   </p>
                 </div>
-                <input id="dropzone-file" type="file" class="hidden" />
+                <input onChange={handleUpload} id="dropzone-file" type="file" class="hidden" />
               </label>
               <div class="absolute w-[20rem] bg-tertiary font-bold text-quaternary text-center text-xs rounded-lg py-2 right-0 bottom-full mb-3 opacity-0 group-hover:opacity-100 transition duration-200 ease-in-out">
                 <span className=" text-white">Field should contain:-</span>{" "}
                 Name, Email, Password, Prn, Branch, Currentsem,Role
               </div>
             </div>
-            
           </div>
         </div>
       </div>
